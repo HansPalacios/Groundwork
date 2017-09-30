3d93e17b810c769100be798bd00f5fe5d620876a
Hans-Sebastian <hanssebastian.p@gmail.com>
2017-09-30 12:52:07 -0400

    mission opaque

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-
+	
 	var myIndex = 0;
 	carousel();
 
 	function carousel() {
 	    var i;
 	    var x = document.getElementsByClassName("mySlides");
+	    if( x )
 	    for (i = 0; i < x.length; i++) {
 	       x[i].style.display = "none";  
 	    }
