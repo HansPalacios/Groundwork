4c577aa6f2d0bf83853502d74cb5be368dd95bf6
Hans-Sebastian <hanssebastian.p@gmail.com>
2017-10-11 17:52:37 -0400

    recent clients

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
 	def index
 		@homepage = true
+		# @review = Review.find(params[:review_id])
+		@reviews = Review.order("created_at desc").limit(4)
 	end
 end
