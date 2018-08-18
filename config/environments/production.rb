1226c2360ada8f039743a7e4b21e0e431cc41503
Hans-Sebastian <hanssebastian.p@gmail.com>
2018-08-18 16:45:46 -0400

    why oh why oh why

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  config.assets.compile = true
+  config.assets.compile = false
 
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
